c2c6fe02699a79ccc04cefaae6dba945c4fdaeba
liuzhuohao <804121985@qq.com>
2023-11-07 23:48:13 +0800

    add bootHookFunction

--- src/main/java/com/wanfeng/myweb/MyWebApplication.java
+++ src/main/java/com/wanfeng/myweb/MyWebApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
 @SpringBootApplication
 @EnableScheduling
 public class MyWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyWebApplication.class, args);
+
     }
 
 }
---
+++ src/main/java/com/wanfeng/myweb/Trig/AfterBoot.java
@@ -0,0 +1,20 @@
+package com.wanfeng.myweb.Trig;
+
+import com.wanfeng.myweb.service.PushIphoneService;
+import com.wanfeng.myweb.vo.PushIphoneVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 项目启动后调用
+ */
+@Component
+public class AfterBoot implements CommandLineRunner {
+    @Autowired
+    private PushIphoneService pushIphoneService;
+    @Override
+    public void run(String... args) throws Exception {
+        pushIphoneService.push(new PushIphoneVo("MyWeb","项目启动成功","MyWeb"));
+    }
+}
