33e41dcf1952c0c8d69d47c181d141f108d3962f
liuzhuohao <804121985@qq.com>
2023-11-08 17:07:30 +0800

    feat killYuanShenDailyJob

--- src/main/java/com/wanfeng/myweb/quartz/dailyJob.java
+++ src/main/java/com/wanfeng/myweb/quartz/dailyJob.java
@@ -1,23 +1,30 @@
 package com.wanfeng.myweb.quartz;
 
 import com.wanfeng.myweb.service.BiliService;
+import com.wanfeng.myweb.service.PushIphoneService;
 import com.wanfeng.myweb.service.YuanShenService;
+import com.wanfeng.myweb.vo.PushIphoneVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
 @Component
-public class BiliQuartz {
+public class dailyJob {
     @Autowired
     private BiliService biliService;
     @Autowired
     private YuanShenService yuanShenService;
+    @Autowired
+    private PushIphoneService pushIphoneService;
     @Scheduled(cron = "0 0 8 * * ? ")
     public void BiliDailyTask(){
        biliService.startBiliTask();
     }
 
     @Scheduled(cron = "0 0 8 * * ? ")
-    public void YuanShenSignDailyTask(){yuanShenService.doSign();}
+    public void YuanShenSignDailyTask(){
+//        yuanShenService.doSign();
+        pushIphoneService.push(new PushIphoneVo("原神","未签到等待风控结束测试","原神"));
+    }
 }
