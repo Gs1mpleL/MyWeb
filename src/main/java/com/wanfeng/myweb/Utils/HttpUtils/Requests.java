1fefa5e41b1a0b7969a6699d8ae9e75d4a437fc6
liuzhuohao <804121985@qq.com>
2023-12-25 17:48:13 +0800

    feat：重构Http工具类

--- src/main/java/com/wanfeng/myweb/Utils/HttpUtils/Requests.java
+++ src/main/java/com/wanfeng/myweb/Utils/HttpUtils/Requests.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -21,24 +22,28 @@ public class Requests {
     public JSONObject get(String url, Map<String, String> map, Map<String,String> headers) {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Content-Type", "application/json;charset=UTF-8");
-        if (headers!=null){
+        if (headers != null) {
             headers.forEach(httpHeaders::add);
         }
-        HttpEntity<String> paramAndHeaderEntity = new HttpEntity<>(generateRequestParameters(url, map), httpHeaders);
-        ResponseEntity<JSONObject> response = restTemplate.getForEntity(url, JSONObject.class, paramAndHeaderEntity);
-        LOGGER.warn("发送请求->[{}],参数->[{}],Header->[{}],结果->[{}]",url,map,headers,response);
+        HttpEntity<String> requestEntity = new HttpEntity<>(null, httpHeaders);
+        url = addParamToUrl(url,map);
+        LOGGER.warn("发送请求->[{}],参数->[{}],Header->[{}]",url,map,httpHeaders);
+        ResponseEntity<JSONObject> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, JSONObject.class);
+        LOGGER.warn("结果->[{}]",response);
         return  response.getBody();
     }
 
     public JSONObject post(String url, Map<String, String> map,Map<String,String> headers) {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Content-Type", "application/json;charset=UTF-8");
-        if (headers!=null){
+        if (headers != null) {
             headers.forEach(httpHeaders::add);
         }
-        HttpEntity<String> paramAndHeaderEntity = new HttpEntity<>(JSONObject.toJSONString(map),httpHeaders);
-        ResponseEntity<JSONObject> response = restTemplate.postForEntity(url, paramAndHeaderEntity,JSONObject.class);
-        LOGGER.warn("发送请求->[{}],参数->[{}],Header->[{}],结果->[{}]",url,map,headers,response);
+        HttpEntity<String> requestEntity = new HttpEntity<>(null, httpHeaders);
+        url = addParamToUrl(url,map);
+        LOGGER.warn("发送请求->[{}],参数->[{}],Header->[{}]",url,map,httpHeaders);
+        ResponseEntity<JSONObject> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, JSONObject.class);
+        LOGGER.warn("结果->[{}]",response);
         return  response.getBody();
     }
 
@@ -47,27 +52,26 @@ public class Requests {
         if (headers!=null){
             headers.forEach(httpHeaders::add);
         }
-        HttpEntity<String> paramAndHeaderEntity = new HttpEntity<>(generateRequestParameters(url, map), httpHeaders);
-        String html = restTemplate.getForObject(url, String.class, paramAndHeaderEntity);
-        LOGGER.warn("发送请求->[{}],参数->[{}],Header->[{}],结果->[{}]",url,map,headers,html);
+        url = addParamToUrl(url,map);
+        LOGGER.warn("发送请求->[{}],参数->[{}],Header->[{}]",url,map,httpHeaders);
+        String html = restTemplate.getForObject(url, String.class, httpHeaders);
+        LOGGER.warn("结果->[{}]",html);
         return html;
     }
 
 /****************************************实现结束***************************************************************/
 
-    public String generateRequestParameters(String uri, Map<String, String> params) {
-        if (params == null) {
-            return uri;
+    public String addParamToUrl(String url, Map<String, String> params) {
+        // 合并params到url上，如果存在的话
+        if (params != null && !params.isEmpty()) {
+            url += "?";
+            StringBuilder urlBuilder = new StringBuilder(url);
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                urlBuilder.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
+            }
+            url = urlBuilder.toString();
+            url = url.substring(0, url.length() - 1); // 去掉最后的"&"
         }
-        StringBuilder sb = new StringBuilder(uri);
-        sb.append("?");
-        for (Map.Entry<String, String> map : params.entrySet()) {
-            sb.append(map.getKey())
-                    .append("=")
-                    .append(map.getValue())
-                    .append("&");
-        }
-        uri = sb.substring(0, sb.length() - 1);
-        return uri;
+        return url;
     }
 }
